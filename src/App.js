7bd88df8fe5c61c284d140fa6e4b007245a33225
hubnotch <love2ekene@gmail.com>
2022-01-01 14:52:40 +0100

    Added styles to the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      
+    <div className="form--container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name=""
+          placeholder="Email address"
+          className="form--input"
+        />
+        <input
+          type="password"
+          name=""
+          placeholder="Password"
+          className="form--input"
+        />
+        <input
+          type="password"
+          name=""
+          placeholder="Confirm Password"
+          className="form--input"
+        />
+        <div className="form--marketing">
+          <input type="checkbox" name="" id="okayToEmail" />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button className="form--submit">Sign up</button>
+      </form>
     </div>
   );
 }
