14da60e4e5bd3d529f423833ae18ae28c3d6440f
hubnotch <love2ekene@gmail.com>
2022-01-02 23:02:40 +0100

    added submit logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    okayToEmail: true,
+  });
+  console.log(formData.email);
+  console.log(formData.password);
+  console.log(formData.confirmPassword);
+  // console.log(formData);
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
   function handleSubmit(event) {
     event.preventDefault();
+    const match =
+      formData.password === formData.confirmPassword
+        ? "Successfully signed up"
+        : "passwords do not match";
+    const isChecked = formData.okayToEmail
+      ? "Thanks for signing up for our newsletter!"
+      : "";
+    console.log(match, isChecked);
   }
   return (
     <div className="form--container">
       <form className="form" onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
+          value={formData.email}
           type="email"
-          name=""
+          name="email"
           placeholder="Email address"
           className="form--input"
         />
         <input
+          onChange={handleChange}
+          value={formData.password}
           type="password"
-          name=""
+          name="password"
           placeholder="Password"
           className="form--input"
         />
         <input
+          onChange={handleChange}
+          value={formData.confirmPassword}
           type="password"
-          name=""
+          name="confirmPassword"
           placeholder="Confirm Password"
           className="form--input"
         />
         <div className="form--marketing">
-          <input type="checkbox" name="" id="okayToEmail" />
+          <input
+            onChange={handleChange}
+            checked={formData.okayToEmail}
+            type="checkbox"
+            name="okayToEmail"
+            id="okayToEmail"
+          />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
         <button className="form--submit">Sign up</button>
